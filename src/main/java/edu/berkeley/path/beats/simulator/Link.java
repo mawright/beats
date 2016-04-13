6a9d621aaa804699df0e54d05b855df33873f365
mawright <mwright@berkeley.edu>
2016-04-12 22:16:55 -0700

    Turn off the HOV restriction until we can make this consistent

--- src/main/java/edu/berkeley/path/beats/simulator/Link.java
+++ src/main/java/edu/berkeley/path/beats/simulator/Link.java
@@ -429,8 +429,8 @@ public class Link extends edu.berkeley.path.beats.jaxb.Link implements Serializa
     // generic method to check permissibility of entrance for vType
     // to be expanded for more cases as needed (including querying controllers)
     public boolean canVTypeEnter(String vType){
-        if(isHov() && vType.compareToIgnoreCase("hov")!=0)
-            return false;
+//        if(isHov() && vType.compareToIgnoreCase("hov")!=0)
+//            return false;
         return true;
     }
 
